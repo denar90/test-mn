8e0ff29b19509c602d25072e5da80dbf2e485f5f
Artem Denysov <denysov.artem@gmail.com>
2016-06-01 13:41:54 +0300

    Update trigger-build.js

--- trigger-build.js
+++ trigger-build.js
@@ -8,12 +8,10 @@ var travis = new Travis({
 })
 
 travis.auth.github.post({
-    github_token: '0e7fefbd5f4759b505ac65d5f07eb0dbb9594cec'
+    github_token: process.env.GH_TOKEN
 }, function (err, res) {
   console.log(err);
-    // res => {
-    //     access_token: XXXXXXX
-    // }
+  
     travis.authenticate({
         access_token: res.access_token
     }, function (err) {

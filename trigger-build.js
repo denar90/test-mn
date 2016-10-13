04a03d728cfb4d2f260390f23ef39481bf87ceb7
Artem Denysov <denysov.artem@gmail.com>
2016-10-14 00:00:41 +0300

    Update trigger-build.js

--- trigger-build.js
+++ trigger-build.js
@@ -29,6 +29,7 @@ travis.authenticate({
 			return console.error(err);
 		}
     	//rebuild latest build
+		console.log(getLastMainBuildId(res.builds));
     	travis.requests.post({
 			build_id: getLastMainBuildId(res.builds)
 		}, function (err, res) {
